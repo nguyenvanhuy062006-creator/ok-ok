a794f1f3296b809e427c40347d45cbd4f1759f6a
Macbook <macbook@Mac-Cua-Huy.local>
2026-06-30 18:08:21 +0700

    cập nhật cuối ngày 1

--- cv-code/src/layout/MainContent.tsx
+++ cv-code/src/layout/MainContent.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import './MainLayout.scss';
+import {useTranslation} from 'react-i18next';
 
 const MainContent: React.FC = () => {
+  const {t}=useTranslation();
+  const experienceList =t('experience_list',{returnObjects: true})as any[];
+  const skills=[
+    {name: 'html',level : 4},
+    {name: 'css', level: 4},
+    {name: 'js', level: 4},
+    {name: 'reactjs', level: 4}
+  ];
+
+
   return (
-    <main className="main-content">
-      <h1 className="main-title">Hello</h1>
+    <main className="cv-main-content">
+      <section className='main-section'>
+        <h3 className='main-section-tittle'><span className='icon-circle'>🧑‍💻</span>{t('about_me')}</h3>
+        <p className ='sumary-text'>{t('about_me_desc')}</p>
+      </section>
+      
     </main>
   );
 };
